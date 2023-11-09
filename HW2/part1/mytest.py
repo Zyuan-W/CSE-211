f2b157a914ba7c708badc6d3e3cbaebd90a7fbba
Ziyuan Wang <53027037+Zyuan-W@users.noreply.github.com>
2023-11-08 17:25:30 -0800

    add mytest

---
+++ HW2/part1/mytest.py
@@ -0,0 +1,25 @@
+
+t = input()
+c = input()
+x = input()
+w = input()
+
+while c:
+    if x:
+        y = input()
+        if w:
+            x = w
+        else:
+            z = y
+    if t:
+        w = y
+
+while c:
+    if t:
+        t = c
+        y = t
+    if c:
+        x = c
+    w = x
+    
+y = w
