08b5b67b38084a37d782376b122a760b882f530b
Ziyuan Wang <53027037+Zyuan-W@users.noreply.github.com>
2023-11-13 14:53:43 -0800

    implemented rpo with reverse CFG

--- HW2/part2/skeleton.py
+++ HW2/part2/skeleton.py
@@ -47,11 +47,15 @@ def extract_variables_from_assignment(instruction):
         return None, None
 
 # Function to create the per-node sets of UEVar and VarKill
-def create_sets(node, instruction):
+def create_uevar_varkill_sets(node, instruction):
     left_var, right_vars = extract_variables_from_assignment(instruction)
     
+    # print("left_var: ", left_var, "  |  right_vars: ", right_vars)
+    
+    # Upward Exposed Variables are those that are used before being defined in the node
     UEVar = set(right_vars) if right_vars else set()
     
+    # Killed Variables are those that are defined and reassigned in the node
     VarKill = {left_var} if left_var else set()
     
     # Remove killed variables from UEVar
@@ -70,8 +74,9 @@ def postorder_traversal(CFG):
             for successor in get_node_successors(CFG,node):
                 visit(successor)
             postorder_list.append(node)
-    
-    visit(CFG.get_node(0)) # Start at the start node
+    entry_node = next(n for n in CFG.nodes() if CFG.in_degree(n) == 0)
+    visit(entry_node)
+    # visit(CFG.get_node(0)) # Start at the start node
     
     return postorder_list
 
@@ -79,33 +84,41 @@ def postorder_traversal(CFG):
 # successor nodes.
 def compute_LiveOut(CFG):
     global iter_counter
+    reverse_CFG = CFG.reverse()
     iter_counter = 0
     LiveOut = {n : set() for n in CFG.nodes()}
+    # LiveOut = {n : set() for n in postorder_traversal(CFG)}
     UEVar = {}
     VarKill = {}
     
-    postorder_nodes = postorder_traversal(CFG)
+    postorder_nodes = postorder_traversal(reverse_CFG)
     rpo_nodes = list(reversed(postorder_nodes))
 
+    # print("rpo_nodes: ", rpo_nodes)
     # First, create per-node UEVar and VarKill sets(Part 2.1)
-    for node in CFG.nodes():
+    # for node in CFG.nodes():
+    for node in rpo_nodes:
         instruction = get_node_instruction(node)
         
         # print("instruction: ", instruction)
-        UEVar[node], VarKill[node] = create_sets(node, instruction)
+        UEVar[node], VarKill[node] = create_uevar_varkill_sets(node, instruction)
         # print("UEVar: ", UEVar[node], "  |  VarKill: ", VarKill[node])
-        
   
     changed = True # Flag - has there been a change in LiveOut?
 
     # Iterate until no changes(fixpoint algorithm)(Part 2.2)
     while changed:
+        # print out the current LiveOut sets
+        # for node in CFG.nodes():
+        #     print(f"LiveOut[{node}]: {LiveOut[node]}")
+        # print("")
+        
         iter_counter += 1
         changed = False
         # for node in list(CFG.nodes()): # default order
         for node in rpo_nodes: # reverse postorder
-
             current_LiveOut = LiveOut[node].copy()
+            # print("current_LiveOut: ", current_LiveOut)
 
             # Compute new LiveOut value 
             for successor in get_node_successors(CFG, node):
@@ -124,6 +137,8 @@ def compute_LiveOut(CFG):
 # function. It simply needs to return a set of uninitialized variables
 def get_uninitialized_variables_from_LiveOut(CFG, LiveOut):
     return LiveOut[CFG.get_node(0)]
+    # entry_node = next(n for n in CFG.nodes() if CFG.in_degree(n) == 0)
+    # return LiveOut[entry_node]
 
 # The testing function. Keep the signature of this function the
 # same as it will be used for grading. I highly recommend you keep the
@@ -203,5 +218,31 @@ if __name__ == '__main__':
 # passed test: 6
 # ---
 # iterations: 8
+# passed test: 7
+# ---
+
+# rpo with reverse CFG
+# iterations: 2
+# passed test: 0
+# ---
+# iterations: 2
+# passed test: 1
+# ---
+# iterations: 2
+# passed test: 2
+# ---
+# iterations: 3
+# passed test: 3
+# ---
+# iterations: 3
+# passed test: 4
+# ---
+# iterations: 3
+# passed test: 5
+# ---
+# iterations: 3
+# passed test: 6
+# ---
+# iterations: 3
 # passed test: 7
 # ---
