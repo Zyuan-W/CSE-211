d1dd84b80bc495c17cead61bc9d78a535bec76b7
Ziyuan Wang <53027037+Zyuan-W@users.noreply.github.com>
2023-11-21 22:31:06 -0800

    add a test script

--- HW3/part1/checker_ind_loop.h
+++ HW3/part1/checker_ind_loop.h
@@ -1,4 +1,14 @@
 #include <assert.h>
+#include <iostream>
 
 void check_values(float *ref, float *actual, int size) {
+    // compare the two arrays, if they are the same, the program will continue,
+    // if not the program will terminate 
+    
+    for (int i = 0; i < size; i++) {
+        assert(ref[i] == actual[i]);
+    }
+
+    std::cout << "Match!" << std::endl;
+
 } 
---
+++ HW3/part1/test_ind.sh
@@ -0,0 +1,3 @@
+for i in 1 2 4 8 16 32 64; do
+    python3 skeleton_ind_loop.py 64 $i
+done
