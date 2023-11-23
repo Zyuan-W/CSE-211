6fe2075a4078c3b0a16751c239b058e67068f3fe
johnyu763 <johnyu763@gmail.com>
2023-11-22 21:29:55 -0800

    parse read and write index

--- HW3/part2/skeleton.py
+++ HW3/part2/skeleton.py
@@ -13,18 +13,52 @@ def get_ast_from_file(fname):
     body_ast = module_ast.body[0]    
     return body_ast
 
+def pp_expr(node):
+    # print(str(node.__class__))
+    # return ast.dump(node)
+    if(str(node.__class__) ==  "<class '_ast.Num'>"):
+        return str(node.n)
+    elif(str(node.__class__) ==  "<class '_ast.Constant'>"):
+        return str(node.n)
+    elif(str(node.__class__) ==  "<class '_ast.Index'>"):
+        return str(pp_expr(node.value))
+    elif(str(node.__class__) ==  "<class '_ast.Name'>"):
+        return str(node.id)
+    elif(str(node.__class__) ==  "<class '_ast.BinOp'>"):
+        return str(pp_expr(node.left)) + str(pp_expr(node.op)) + str(pp_expr(node.right)) 
+    elif(str(node.__class__) ==  "<class '_ast.Mult'>"):
+        return '*'
+    elif(str(node.__class__) ==  "<class '_ast.Add'>"):
+        return '+'
+    elif(str(node.__class__) ==  "<class '_ast.Div'>"):
+        return '/'
+    elif(str(node.__class__) ==  "<class '_ast.Sub'>"):
+        return '-'
+    
 # Example of how to get information for the ast FOR_node
 def get_loop_constraints(FOR_node):
     loop_var = FOR_node.target.id
-    lower_bound = pp_expr(FOR_node.iter.args[0])
-    upper_bound = pp_expr(FOR_node.iter.args[1])
-
+    lower_bound = FOR_node.iter.args[0].n #pp_expr(FOR_node.iter.args[0])
+    upper_bound = FOR_node.iter.args[1].n #pp_expr(FOR_node.iter.args[1])
+    print(lower_bound, upper_bound)
     # return the for loop information in some structure
-    # return (loop_var, lower_bound, upper_bound)
+    return (loop_var, lower_bound, upper_bound)
 
 # Check if an ast node is a for loop
 def is_FOR_node(node):
     return str(node.__class__) == "<class '_ast.For'>"
+
+# takes in assign node
+def get_write_info(node):
+    name = pp_expr(node.targets[0].value)
+    index = pp_expr(node.targets[0].slice)
+    return (name, index)
+
+# takes in assign node
+def get_read_info(node):
+    name = pp_expr(node.value.value)
+    index = pp_expr(node.value.slice)
+    return (name, index)
 
 # Top level function. Given a python file name, it parses the file,
 # and analyzes it to determine if the top level for loop can be done
@@ -34,8 +68,37 @@ def is_FOR_node(node):
 # otherwise it returns False.
 def analyze_file(fname):
 
-    ast = get_ast_from_file(fname)
+    my_ast = get_ast_from_file(fname)
+    # print(ast)
+    loop_var, lower_bound, upper_bound = get_loop_constraints(my_ast)
+    my_expr = my_ast
 
+    while is_FOR_node(my_expr):
+        my_expr = my_expr.body[0]
+    print(ast.dump(my_expr))
+    print(get_write_info(my_expr))
+    print(get_read_info(my_expr))
+    # print(ast.dump(my_ast))
+    # print(my_ast.body[0])
+    # print(my_ast.body[0].targets)
+    # print(my_ast.body[0].value)
+    # print(my_ast.body[0].targets[0].value.id)
+    # print(my_ast.body[0].value.value.id)
+    # print(my_ast.body[0].targets[0].slice.value)
+    # write_target = my_expr.targets[0].value.id
+    # read_target = my_expr.value.value.id
+    # write_index = my_expr.targets[0].slice.value.id
+    # read_index = my_expr.value.slice.value.n
+    # print(my_expr.targets[0].slice.value.id)
+    # print(my_expr.value.slice.value.n)
+    # ix = Int('ix')
+    # iy = Int('iy')
+    # write-write
+    # print(ix + iy)
+    # mysolver = z3.Solver()
+    # mysolver.solve(1 != 2)
+    # z3.solve(ix != iy, ix >= lower_bound, ix < upper_bound, iy >= lower_bound, iy < upper_bound )
+    
     # Suggested steps:
     # 1. Get loop constraints (variables and bounds)
     # 2. Get expressions for read_index and write_index
