e7b598107b8127cec9d152665c3c9f68b0befebf
johnyu763 <johnyu763@gmail.com>
2023-11-25 09:05:31 -0800

    part 2 code

--- HW3/part2/skeleton.py
+++ HW3/part2/skeleton.py
@@ -13,33 +13,35 @@ def get_ast_from_file(fname):
     body_ast = module_ast.body[0]    
     return body_ast
 
-def pp_expr(node):
+def pp_expr(node, numbering=None):
     # print(str(node.__class__))
     # return ast.dump(node)
     if(str(node.__class__) ==  "<class '_ast.Num'>"):
-        return str(node.n)
+        return node.n
     elif(str(node.__class__) ==  "<class '_ast.Constant'>"):
-        return str(node.n)
+        return node.n
     elif(str(node.__class__) ==  "<class '_ast.Index'>"):
-        return str(pp_expr(node.value))
+        return pp_expr(node.value, numbering)
     elif(str(node.__class__) ==  "<class '_ast.Name'>"):
-        return str(node.id)
+        if(numbering == None):
+          return z3.Int(node.id)
+        return numbering
     elif(str(node.__class__) ==  "<class '_ast.BinOp'>"):
-        return str(pp_expr(node.left)) + str(pp_expr(node.op)) + str(pp_expr(node.right)) 
-    elif(str(node.__class__) ==  "<class '_ast.Mult'>"):
-        return '*'
-    elif(str(node.__class__) ==  "<class '_ast.Add'>"):
-        return '+'
-    elif(str(node.__class__) ==  "<class '_ast.Div'>"):
-        return '/'
-    elif(str(node.__class__) ==  "<class '_ast.Sub'>"):
-        return '-'
+        if(str(node.op.__class__) ==  "<class '_ast.Mult'>"):
+            return pp_expr(node.left, numbering) * pp_expr(node.right, numbering) 
+        if(str(node.op.__class__) ==  "<class '_ast.Add'>"): 
+            return pp_expr(node.left, numbering) + pp_expr(node.right, numbering) 
+        if(str(node.op.__class__) ==  "<class '_ast.Div'>"):
+            return pp_expr(node.left, numbering) / pp_expr(node.right, numbering) 
+        if(str(node.op.__class__) ==  "<class '_ast.Sub'>"): 
+            return pp_expr(node.left, numbering) - pp_expr(node.right, numbering) 
+        
     
 # Example of how to get information for the ast FOR_node
 def get_loop_constraints(FOR_node):
     loop_var = FOR_node.target.id
-    lower_bound = FOR_node.iter.args[0].n #pp_expr(FOR_node.iter.args[0])
-    upper_bound = FOR_node.iter.args[1].n #pp_expr(FOR_node.iter.args[1])
+    lower_bound = pp_expr(FOR_node.iter.args[0])
+    upper_bound = pp_expr(FOR_node.iter.args[1])
     print(lower_bound, upper_bound)
     # return the for loop information in some structure
     return (loop_var, lower_bound, upper_bound)
@@ -49,16 +51,16 @@ def is_FOR_node(node):
     return str(node.__class__) == "<class '_ast.For'>"
 
 # takes in assign node
-def get_write_info(node):
-    name = pp_expr(node.targets[0].value)
-    index = pp_expr(node.targets[0].slice)
-    return (name, index)
+def get_write_info(node, numbering):
+    # name = pp_expr(node.targets[0].value, numbering)
+    index = pp_expr(node.targets[0].slice, numbering)
+    return index
 
 # takes in assign node
-def get_read_info(node):
-    name = pp_expr(node.value.value)
-    index = pp_expr(node.value.slice)
-    return (name, index)
+def get_read_info(node, numbering):
+    # name = pp_expr(node.value.value, numbering)
+    index = pp_expr(node.value.slice, numbering)
+    return index
 
 # Top level function. Given a python file name, it parses the file,
 # and analyzes it to determine if the top level for loop can be done
@@ -72,31 +74,39 @@ def analyze_file(fname):
     # print(ast)
     loop_var, lower_bound, upper_bound = get_loop_constraints(my_ast)
     my_expr = my_ast
-
+    # print(str(None))
     while is_FOR_node(my_expr):
         my_expr = my_expr.body[0]
     print(ast.dump(my_expr))
-    print(get_write_info(my_expr))
-    print(get_read_info(my_expr))
-    # print(ast.dump(my_ast))
-    # print(my_ast.body[0])
-    # print(my_ast.body[0].targets)
-    # print(my_ast.body[0].value)
-    # print(my_ast.body[0].targets[0].value.id)
-    # print(my_ast.body[0].value.value.id)
-    # print(my_ast.body[0].targets[0].slice.value)
-    # write_target = my_expr.targets[0].value.id
-    # read_target = my_expr.value.value.id
-    # write_index = my_expr.targets[0].slice.value.id
-    # read_index = my_expr.value.slice.value.n
-    # print(my_expr.targets[0].slice.value.id)
-    # print(my_expr.value.slice.value.n)
-    # ix = Int('ix')
-    # iy = Int('iy')
+    i0 = z3.Int('i0')
+    i1 = z3.Int('i1')
+    write_index0 = get_write_info(my_expr, i0)
+    write_index1 = get_write_info(my_expr, i1)
+    read_index0 = get_read_info(my_expr, i0)
+    read_index1 = get_read_info(my_expr, i1)
+    print(write_index0, write_index1)
+    # print(write_name0)
+    print(read_index0, read_index1)
+   
+
     # write-write
     # print(ix + iy)
-    # mysolver = z3.Solver()
-    # mysolver.solve(1 != 2)
+    mysolver = z3.Solver()
+    mysolver.add(i0 != i1)
+    mysolver.add(i0 >= lower_bound)
+    mysolver.add(i1 >= lower_bound)
+    mysolver.add(i0 < upper_bound)
+    mysolver.add(i1 < upper_bound)
+    mysolver.add(write_index0 == write_index1)
+    print(mysolver.check())
+    mysolver2 = z3.Solver()
+    mysolver2.add(i0 != i1)
+    mysolver2.add(i0 >= lower_bound)
+    mysolver2.add(i1 >= lower_bound)
+    mysolver2.add(i0 < upper_bound)
+    mysolver2.add(i1 < upper_bound)
+    mysolver2.add(write_index0 == read_index1)
+    print(mysolver2.check())
     # z3.solve(ix != iy, ix >= lower_bound, ix < upper_bound, iy >= lower_bound, iy < upper_bound )
     
     # Suggested steps:
@@ -106,8 +116,8 @@ def analyze_file(fname):
 
     # Set these variables to True if there is a write-write (ww)
     # conflict or a read-write (rw) conflict
-    ww_conflict = False
-    rw_conflict = False
+    ww_conflict = str(mysolver.check()) == 'sat'
+    rw_conflict = str(mysolver2.check()) == 'sat'
     
     return ww_conflict, rw_conflict
     
