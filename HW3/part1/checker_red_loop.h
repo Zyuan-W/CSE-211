b70cbac229c1e056df12195e5f10ab7a699c30c8
Ziyuan Wang <53027037+Zyuan-W@users.noreply.github.com>
2023-11-26 20:03:56 -0800

    add test script for part1

--- HW3/part1/checker_red_loop.h
+++ HW3/part1/checker_red_loop.h
@@ -1,4 +1,9 @@
 #include <assert.h>
+#include <iostream>
 
 void check_values(double *ref, double *actual, int size) {
+    
+    assert(ref[0] == actual[0]);
+    
+    std::cout << "Match!\n";
 } 
