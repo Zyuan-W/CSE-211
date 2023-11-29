41f299bbcb121af01e91eea06514800521989bdc
Ziyuan Wang <53027037+Zyuan-W@users.noreply.github.com>
2023-11-29 13:28:40 -0800

    add a sample c++ program

---
+++ final_project/sample.cpp
@@ -0,0 +1,47 @@
+
+
+#include <iostream>
+using namespace std;
+
+// Function declaration
+int addNumbers(int a, int b);
+
+int main() {
+    // Variable declarations and assignments
+    int x = 10;
+    int y = 20;
+    int sum;
+
+    // Calling a function
+    sum = addNumbers(x, y);
+
+    // If-else statement
+    if (sum > 20) {
+        cout << "Sum is greater than 20." << endl;
+    } else {
+        cout << "Sum is less than or equal to 20." << endl;
+    }
+
+    // For loop
+    cout << "For loop: ";
+    for (int i = 0; i < 5; i++) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    // While loop
+    cout << "While loop: ";
+    int j = 0;
+    while (j < 5) {
+        cout << j << " ";
+        j++;
+    }
+    cout << endl;
+
+    return 0;
+}
+
+// Function definition
+int addNumbers(int a, int b) {
+    return a + b;
+}
